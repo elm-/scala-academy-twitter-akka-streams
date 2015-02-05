0ac955206b8e461c148b7b28b408967f5b98578e
Elmar Weber <elmar.weber@cupenya.com>
2015-02-05 17:44:44 +0100

    - twitter streamer actor

---
+++ src/main/scala/core/TwitterStreaming.scala
@@ -0,0 +1,47 @@
+package core
+
+import spray.httpx.unmarshalling.{MalformedContent, Unmarshaller, Deserialized}
+import spray.http._
+import spray.json._
+import spray.client.pipelining._
+import akka.actor.{ActorRef, Actor}
+import spray.http.HttpRequest
+import scala.io.Source
+import scala.util.Try
+import spray.can.Http
+import akka.io.IO
+
+trait TwitterAuthorization {
+  def authorize: HttpRequest => HttpRequest
+}
+
+trait OAuthTwitterAuthorization extends TwitterAuthorization {
+  import OAuth._
+  val home = System.getProperty("user.home")
+  val lines = Source.fromFile(s"$home/.twitter-secrets").getLines().toList
+
+  val consumer = Consumer(lines(0), lines(1))
+  val token = Token(lines(2), lines(3))
+
+  val authorize: (HttpRequest) => HttpRequest = oAuthAuthorizer(consumer, token)
+}
+
+object TweetStreamerActor {
+  val twitterUri = Uri("https://stream.twitter.com/1.1/statuses/filter.json")
+}
+
+class TweetStreamerActor(uri: Uri, processor: ActorRef) extends Actor {
+  this: TwitterAuthorization =>
+  val io = IO(Http)(context.system)
+
+  def receive: Receive = {
+    case query: String =>
+      val body = HttpEntity(ContentType(MediaTypes.`application/x-www-form-urlencoded`), s"track=$query")
+      val rq = HttpRequest(HttpMethods.POST, uri = uri, entity = body) ~> authorize
+      sendTo(io).withResponsesReceivedBy(self)(rq)
+    case ChunkedResponseStart(_) =>
+    case MessageChunk(entity, _) =>
+      processor ! entity
+    case _ =>
+  }
+}
